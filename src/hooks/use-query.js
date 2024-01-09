79a6b16aa30c980f57e83d48db7803b96970cea6
Given <givenchyampulembang01@gmail.com>
2024-01-10 00:58:44 +0900

    Feat: Setup projects

---
+++ src/hooks/use-query.js
@@ -0,0 +1,31 @@
+import Cookies from "js-cookie";
+import useSWR from "swr";
+
+export default function UseQuery(url){
+
+    const {error, data, isLoading} = useSWR(
+        url,
+        (url) => fetch(url,{
+            headers : {
+                Authorization : `Bearer ${Cookies.get('token')}`
+            }
+        }).then(async res => {
+            
+            if(res.status === 401){
+                console.log("logout sih ini");
+            }
+
+            if(res.status > 401) throw(await res.json());
+            
+            return res.json();
+    
+        }),
+        {
+            revalidateOnFocus: true,
+            refreshInterval : 50000
+        }
+    );
+
+    return {...data, error, isLoading};
+
+}
